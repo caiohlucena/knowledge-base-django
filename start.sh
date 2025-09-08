65233e89058b7ca9b78e5f50c64b9e6ed0733503
Caio Lucena <caio.hlucena@hotmail.com>
2025-09-07 22:16:34 -0300

    Adiciona script start.sh para deploy no Railway

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-set -e
-
-# Aplicar migrações
 python manage.py migrate --noinput
-
-# Coletar arquivos estáticos
 python manage.py collectstatic --noinput
-
-# Iniciar servidor
-gunicorn knowledge.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn dicionario_financeiro.wsgi:application --bind 0.0.0.0:$PORT
